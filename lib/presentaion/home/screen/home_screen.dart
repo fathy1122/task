2fd01511b7b277e08d9aaebeb27f41936dc44658
fathy1122 <fathyeid364@gmail.com>
2025-06-25 22:28:54 +0200

    create home screen add show model bottom sheet

--- lib/presentaion/home/screen/home_screen.dart
+++ lib/presentaion/home/screen/home_screen.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:task/data/api/api_manager.dart';
 import 'package:task/presentaion/home/widgets/card_widget.dart';
 
-
-
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
   static const routeName = '/';
@@ -18,30 +15,56 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child:FutureBuilder(future: ApiManager.getItems(), builder: (_, snapshot) {
-          if(snapshot.hasError){
-          return Center(child: Text(snapshot.error.toString()));
-
-          }else if(snapshot.hasData){
+          child: FutureBuilder(
+        future: ApiManager.getItems(),
+        builder: (_, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          } else if (snapshot.hasData) {
             var data = snapshot.data;
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
                     crossAxisSpacing: 5,
-                    childAspectRatio: 85/150,
+                    childAspectRatio: 75 / 150,
                     mainAxisSpacing: 5),
-                itemCount:data?.length,
+                itemCount: data?.length,
                 itemBuilder: (context, index) {
-                  return CardWidget(title: data?[index].title??'',price: data?[index].price.toString()??'',image: data?[index].image??'',rating: data?[index].rating?.rate.toString()??'',desc: data?[index].description??'',);
-
-                },),
+                  return InkWell(
+                      onTap: () {
+                        showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return  CardWidget(
+                              title: data?[index].title ?? '',
+                              price: data?[index].price.toString() ?? '',
+                              image: data?[index].image ?? '',
+                              rating: data?[index].rating?.rate.toString() ?? '',
+                              desc: data?[index].description ?? '',
+                              category: data?[index].category ?? '',
+                              count: data?[index].rating?.count.toString() ?? '',
+                            );
+                          },
+                        );
+                      },
+                      child: CardWidget(
+                        title: data?[index].title ?? '',
+                        price: data?[index].price.toString() ?? '',
+                        image: data?[index].image ?? '',
+                        rating: data?[index].rating?.rate.toString() ?? '',
+                        count: data?[index].rating?.count.toString() ?? '',
+                        category: data?[index].category ?? '',
+                      ));
+                },
+              ),
             );
-          }else{
+          } else {
             return const Center(child: CircularProgressIndicator());
           }
-        },)
-      ),
+        },
+      )),
     );
   }
 }
